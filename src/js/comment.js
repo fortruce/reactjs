30b604348fcef163108edd8ac9122b6e041a82e0
joseph <>
2015-02-26 12:55:59 -0500

    populate comments with polling AJAX call

--- src/js/comment.js
+++ src/js/comment.js
@@ -4,11 +4,30 @@ var data = [
 ];
 
 var CommentBox = React.createClass({
+  loadCommentsFromServer: function () {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function() {
+    this.loadCommentsFromServer();
+    setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+  },
   render: function() {
     return (
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList data={this.props.data} />
+        <CommentList data={this.state.data} />
         <CommentForm />
       </div>
     );
@@ -58,6 +77,6 @@ var Comment = React.createClass({
   });
 
 React.render(
-  <CommentBox data={data}/>,
+  <CommentBox url="data.json" pollInterval={2000} />,
   document.getElementById('content')
 );
