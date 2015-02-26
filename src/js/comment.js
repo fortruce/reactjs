b79bfdfabe3882dc9b6a9dcad35825f41357e287
fortruce <>
2015-02-26 11:31:24 -0500

    receive data from props instead of hardcoding the comments

--- src/js/comment.js
+++ src/js/comment.js
@@ -1,9 +1,14 @@
+var data = [
+  {author: "Pete Hunt", text: "This is one comment."},
+  {author: "Jordan Walke", text: "This is *another* comment."}
+];
+
 var CommentBox = React.createClass({
   render: function() {
     return (
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList />
+        <CommentList data={this.props.data} />
         <CommentForm />
       </div>
     );
@@ -12,10 +17,16 @@ var CommentBox = React.createClass({
 
 var CommentList = React.createClass({
   render: function() {
+    var commentNodes = this.props.data.map(function (comment) {
+      return (
+        <Comment author={comment.author}>
+          {comment.text}
+        </Comment>
+          );
+      });
     return (
       <div className="commentList">
-        <Comment author="Pete Hunt">This is one comment</Comment>
-        <Comment author="Jordan Walke">This is *another* comment</Comment>
+        {commentNodes}
       </div>
         );
     }
@@ -47,6 +58,6 @@ var Comment = React.createClass({
   });
 
 React.render(
-  <CommentBox />,
+  <CommentBox data={data}/>,
   document.getElementById('content')
 );
